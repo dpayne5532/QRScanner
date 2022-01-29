c5642160186b31791860975f5fce1d688adce10d
Dan Payne <dpayne5532@gmail.com>
2022-01-29 05:08:59 -0700

    Initial Commit

---
+++ QRScanner/QRScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRScannerApp.swift
+//  QRScanner
+//
+//  Created by Dan Payne on 1/29/22.
+//
+
+import SwiftUI
+
+@main
+struct QRScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
